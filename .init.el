a67e705349fcd5a4149ab066f633c55fdd4b00bf
Sandesh Soni <sonisandesh@gmail.com>
2014-04-27 23:40:29 +0530

    autoload lisp folder

--- .init.el
+++ .init.el
@@ -2,3 +2,8 @@
 (when (version< emacs-version "24.1")
   (error "Soni requires at least GNU Emacs 24.1, but you're running %s" emacs-version))
 
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
