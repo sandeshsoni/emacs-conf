8f5b4cc55554a0dd7926d8969c5ae5c0728508c9
Sandesh Soni <sandeshsoni1@gmail.com>
2014-05-02 00:10:10 +0530

    added guide key

---
+++ lisp/init-editing-utils.el
@@ -0,0 +1,5 @@
+(require-package 'guide-key)
+(setq guide-key/guide-key-sequence '("C-x r" "C-c r" "C-c ;" "C-c ; f" "C-c ' f" "C-x n"))
+(guide-key-mode 1)  ; Enable guide-key-mode
+
+(provide 'init-editing-utils)
