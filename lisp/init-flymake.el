094c40c194358b80ebfb74419b4b85e83d11f641
Sandesh Soni <sonisandesh@gmail.com>
2014-05-01 21:36:32 +0530

    flycheck trigger automatically

--- lisp/init-flymake.el
+++ lisp/init-flymake.el
@@ -1,4 +1,8 @@
 (require-package 'flymake-ruby)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 
+;; Override default flycheck triggers
+(setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+      flycheck-idle-change-delay 0.8)
+
 (provide 'init-flymake)
