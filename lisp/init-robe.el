fb9ba66c1dcd23fa02b0a30e21dc976691b337f3
Sandesh Soni <sonisandesh@gmail.com>
2014-05-01 21:35:45 +0530

    add company mode

--- lisp/init-robe.el
+++ lisp/init-robe.el
@@ -1,7 +1,12 @@
 (require-package 'robe)
+(require-package 'company)
+
 (add-hook 'ruby-mode-hook 'robe-mode)
 
-(defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
-  (rvm-activate-corresponding-ruby))
+;; (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+;;   (rvm-activate-corresponding-ruby))
+(global-company-mode t)
+(push 'company-robe company-backends)
+
 
 (provide 'init-robe)
