f58f0429915e9938fa00bdd7dbac654e170d95c3
Sandesh Soni <sandeshsoni1@gmail.com>
2014-05-05 00:20:53 +0530

    git gutter

---
+++ lisp/init-git.el
@@ -0,0 +1,9 @@
+(require-package 'git-gutter+)
+(require-package 'git-gutter-fringe+)
+(global-git-gutter+-mode t)
+;; (set-face-foreground 'git-gutter-fr+-modified "yellow")
+;; (set-face-foreground 'git-gutter-fr+-added    "blue")
+;; (set-face-foreground 'git-gutter-fr+-deleted  "white")
+
+
+(provide 'init-git)
