946c7d1b5b013f1b22d005869afe8d71f84443cd
Sandesh Soni <sandeshsoni1@gmail.com>
2014-05-05 00:22:01 +0530

    implemented git gutte

--- init.el
+++ init.el
@@ -12,16 +12,6 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 ;;(require 'auto-install)
 
-;;----------------------------------------------------------------------------
-;; Handier way to add modes to auto-mode-alist
-;;----------------------------------------------------------------------------
-;; (defun add-auto-mode (mode &rest patterns)
-;;   "Add entries to `auto-mode-alist' to use `MODE' for all given file `PATTERNS'."
-;;   (dolist (pattern patterns)
-;;     (add-to-list 'auto-mode-alist (cons pattern mode))))
-
-
-
 (require 'init-package)    ;; Machinery for installing required packages
 (require 'init-ido)
 (require 'init-flymake)
@@ -38,5 +28,6 @@
 (require-package 'htmlize)
 
 (require 'init-custom-bindings)
+(require 'init-git)
 
 (provide 'init)
