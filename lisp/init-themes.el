169480a08c7a67b74069a0e108cbcc30e07f4b3e
Sandesh Soni <sandeshsoni1@gmail.com>
2014-05-04 22:58:53 +0530

    theme changer package

---
+++ lisp/init-themes.el
@@ -0,0 +1,13 @@
+(require 'cl-lib)
+(require-package 'theme-changer)
+
+;; (setq calendar-location-name "Mumbai, IN")
+;; (setq calendar-latitude 18.52)
+;; (setq calendar-longitude 73.85)
+
+;;(change-theme 'color-theme-solarized-light 'color-theme-wombat)
+;;(add-to-list 'load-path "~/.emacs.d/elisp/theme-changer")
+;;(load-theme 'wombat t)
+;;(change-theme 'wombat 'wombat)
+
+(provide 'init-themes)
