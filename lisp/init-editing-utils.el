7b69bd6a265ebcfcd1e28ff158098fd664a0fe09
Sandesh Soni <sandeshsoni1@gmail.com>
2014-05-11 14:49:04 +0530

    add package move-text with binding

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -2,4 +2,15 @@
 (setq guide-key/guide-key-sequence '("C-x r" "C-c r" "C-c ;" "C-c ; f" "C-c ' f" "C-x n"))
 (guide-key-mode 1)  ; Enable guide-key-mode
 
+;;----------------------------------------------------------------------------
+;; Shift lines up and down with M-up and M-down. When paredit is enabled,
+;; it will use those keybindings. For this reason, you might prefer to
+;; use M-S-up and M-S-down, which will work even in lisp modes.
+;;----------------------------------------------------------------------------
+(require-package 'move-text)
+(global-set-key [M-up] 'move-text-up)
+(global-set-key [M-down] 'move-text-down)
+(global-set-key [M-S-up] 'move-text-up)
+(global-set-key [M-S-down] 'move-text-down)
+
 (provide 'init-editing-utils)
